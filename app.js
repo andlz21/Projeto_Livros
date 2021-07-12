4f792528381c12b6b890bf248aedb9ff7cd76299
andlz21 <alsc989@yahoo.com.br>
2021-07-12 19:35:03 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const livros = require('./database')
+
+// pegar input de pessoa usuária por categoria
+
+// Se for SIM, mostra as categorias disponíveis, pergunta qual ela escolhe
+
+// Se for NÃO, mostra todos os livros em ordem crescente das quantidades de páginas
+
+
+const readline = require('readline-sync')
+
+const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+
+
+if (entradaInicial.toLocaleUpperCase( ) ===  'S') {
+    console.log('Essas são as categorias disponíveis: ')
+    console.log('Produtividade e estilo de vida','/Tecnologia','/Games')
+    
+    const entradaCategoria = readline.question('Qual categoria você deseja: ')
+
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno)
+
+} else {
+
+  const livrosOrdenados = livros.sort((a,b) => a.paginas - b.paginas)
+  console.log('Essas são todos os livros disponíveis: ')
+  console.table(livrosOrdenados)
+}
+
+
---
+++ database.js
@@ -0,0 +1,58 @@
+const livros = [
+  {
+          id: 1,
+          nome: "Digital Minimalism", 
+          autor: "Cal Newport",
+          categoria: "Produtividade e estilo de vida",
+          paginas: 254,
+          recomenda: false,
+          leu: false
+  },
+  {
+          id: 2,
+          nome: "New digital world", 
+          autor: "Antony Jones",
+          categoria: "Tecnologia",
+          paginas: 285,
+          recomenda: false,
+          leu: false
+    },
+    {
+          id: 3,
+          nome: "The future jobs", 
+          autor: "Steve jobs",
+          categoria: "Produtividade e estilo de vida",
+          paginas: 222,
+          recomenda: false,
+          leu: false
+      },
+      {
+          id: 4,
+          nome: "Gestão de alta performance", 
+          autor: "Andre S. Grove",
+          categoria: "Produtividade e estilo de vida",
+          paginas: 272,
+          recomenda: false,
+          leu: false
+        },
+        {
+          id: 5,
+          nome: "Padrões Javascript", 
+          autor: "Stoyan Stefanov",
+          categoria: "Tecnologia",
+          paginas: 254,
+          recomenda: true,
+          leu: true
+          },
+          {
+            id: 6,
+            nome: "A Guerra Dos Consoles", 
+            autor: "Blake J. Harris",
+            categoria: "Games",
+            paginas: 558,
+            recomenda: true,
+            leu: true
+            },        
+]
+
+module.exports = livros
